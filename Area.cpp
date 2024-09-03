c22013cd4cdfe540ad64269a48f1553fdcf1a2c0
hinson001 <hinsonho001@gmail.com>
2024-09-03 13:21:06 +0800

    area

---
+++ Area.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int w, h = 0;
+    cout << "Please enter the width\n";
+    cin >> w;
+    cout << "Please enter the height\n";
+    cin >> h;
+    cout << "The area is " << w << "*" << h << "=" << w*h << endl;
+    return 0;
+}
